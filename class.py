5f914b86247b4a6c7560c7b25381f0a2dc78dd4c
Kudin <91375108+Alexander18101995@users.noreply.github.com>
2022-05-20 18:11:04 +0300

    Add files via upload

--- class.py
+++ class.py
@@ -1,65 +1,65 @@
-grades_git = [5, 4, 3, 2]
-grades_python = [5, 4, 3, 2]
+grades_lecturer_git = [5, 4, 5, 3]
+grades_lecturer_python = [5, 4, 3, 4]
+grades_student_git = [5, 4, 2, 3]
+grades_student_python = [4, 2, 3, 2]
 courses = ('Git','Python')
 cool_mentor = ('Some', 'Buddy')
 best_student = ('Ruoy', 'Eman')
-def avg_grades_git(grades):
+def avg_grades_lecturer_git(grades):
       sum_ = 0
       for git in grades:
         sum_ =+ sum(grades) / len(grades)
-        return round(sum(grades) / len(grades), 0)
-def avg_grades_python(grades):
+        return round(sum(grades) / len(grades), 2)
+def avg_grades_lecturer_python(grades):
       sum_ = 0
       for python in grades:
         sum_ =+ sum(grades) / len(grades)
-        return round(sum(grades) / len(grades), 0)        
+        return round(sum(grades) / len(grades), 2) 
 def avg_grades(grades_git,grades_python):
   grades= (grades_git + grades_python) / 2
   return grades
-
+def avg_grades_student_git(grades):
+      sum_ = 0
+      for git in grades:
+        sum_ =+ sum(grades) / len(grades)
+        return round(sum(grades) / len(grades), 2)
+def avg_grades_student_python(grades):
+      sum_ = 0
+      for python in grades:
+        sum_ =+ sum(grades) / len(grades)
+        return round(sum(grades) / len(grades), 2) 
+def avg_grades(grades_git,grades_python):
+  grades= (grades_git + grades_python) / 2
+  return grades
 class Student:
-    def __init__(self, name, surname):
+    def __init__(self, name, surname, avg_grades):
         self.name = name
         self.surname = surname
         self.finished_courses = []
         self.courses_in_progress = []
-        self.grades = []
+        self.avg_grades = avg_grades
         self.lecrurer_grades = []
 
     def __str__(self):
-      return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {avg_grades(avg_grades_git(grades_git),avg_grades_git(grades_python))}\nКурсы в процессе изучения: {courses[0]}, {courses[1]}\nЗавершенные курсы: Введение в программирование '
+      return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {self.avg_grades}\nКурсы в процессе изучения: {courses[0]}, {courses[1]}\nЗавершенные курсы: Введение в программирование '
 
+    def __lt__(self, other):
+      return self.avg_grades < other.avg_grades
 
-
-some_student = Student(best_student[0],best_student[1])
-class Mentor:
-    def __init__(self, name, surname):
-        self.name = name
-        self.surname = surname
-        self.courses_attached = []
-
-    def __str__(self):
-      return f'Имя: {self.name}\nФамилия: {self.surname}'
-name_mentor = Mentor(cool_mentor[0],cool_mentor[1])
+some_student = Student(best_student[0],best_student[1],avg_grades(avg_grades_student_git(grades_student_git),avg_grades_student_python(grades_student_python)))
 class Lecturer:
-    def __init__ (self,name,last_name):
+    def __init__ (self,name,last_name,avg_grades):
         self.name = name
         self.last_name = last_name 
+        self.avg_grades = avg_grades
     def __str__(self):
-      return f'Имя: {self.name}\nФамилия: {self.last_name}\nСредняя оценка за лекции: {avg_grades(avg_grades_git(grades_git),avg_grades_git(grades_python))}'
-some_lecturer = Lecturer(cool_mentor[0],cool_mentor[1])
-class Reviewe(Mentor):
-    def __init__ (self,name,last_name):
-        self.name = name
-        self.last_name = last_name
-        self.student_grades
+      return f'Имя: {self.name}\nФамилия: {self.last_name}\nСредняя оценка за лекции: { self.avg_grades }'
+some_lecturer = Lecturer(cool_mentor[0],cool_mentor[1],avg_grades(avg_grades_lecturer_git(grades_lecturer_git),avg_grades_lecturer_python(grades_lecturer_python)))
+def __lt__(self, other):
+  return self.avg_grades < other.avg_grades
 
-    def __str__(self):
-      return f'Имя: {self.name}\nФамилия: {self.surname}'
-some_reviewe = Mentor(cool_mentor[0],cool_mentor[1])
-
-print(some_reviewe)
 print()
 print(some_lecturer)
 print()
 print(some_student)
+print(some_student < some_lecturer )
